0d9e2e61ffda0c8a25245cc72354eeb702fea841
TitanChariotsBB <cmagwoodva@gmail.com>
2024-10-17 09:41:22 -0400

    Added new mode to Oscillator and tested it

--- oscillator.py
+++ oscillator.py
@@ -1,5 +1,6 @@
 import random
 import numpy as np
+import math
 
 # Multi voice additive oscillator
 class Oscillator:
@@ -20,10 +21,31 @@ class Oscillator:
             self.amps[i] = random.random()
             
             # random frequency [40, Nyquist)
-            freq = random.random() * ((self.sample_rate / 2) - 40) + 40
+            # scaled logarithmically
+            freq = math.pow(2, ((10 * random.random()) - 10)) * ((self.sample_rate / 2) - 40) + 40
             self.freqs[i] = freq
             inc = 2 * np.pi * freq / self.sample_rate
             self.incs[i] = inc
+        return self.freqs
+
+    def overtones(self, fundamental_freq):
+        # fundamental
+        self.freqs[0] = fundamental_freq
+        self.amps[0] = 1.0
+        self.incs[0] = 2 * np.pi * fundamental_freq / self.sample_rate
+        
+        for i in range(1, self.num_oscs):
+            # random amplitude [0, 1)
+            self.amps[i] = random.random()
+            
+            # ith harmonic
+            freq = (i + 1) * fundamental_freq
+            if (freq > (self.sample_rate / 2)): # don't go over the Nyquist
+                break
+            self.freqs[i] = freq
+            inc = 2 * np.pi * freq / self.sample_rate
+            self.incs[i] = inc
+            
             
     def nextSample(self):
         sum = 0.0
